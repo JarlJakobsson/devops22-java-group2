13b1311244442591a1f0d2ae5ad31baec61cac0d
Tolli-txt <tolli.txt@gmail.com>
2023-01-13 15:27:31 +0100

    Removed cow, added goat (All contributed)

--- demo/src/main/java/Y/Main.java
+++ demo/src/main/java/Y/Main.java
@@ -6,8 +6,10 @@ abstract class Main {
     public static void main( String[] args )
     {
         Player player = new Player("Milkman");
-        Cow cow = new Cow();
-        player.buyAnimal(cow);
+        // Cow cow = new Cow();
+        Goat goat = new Goat();
+        System.out.println("Milk value:" + goat.getMilkValue());
+        player.buyAnimal(goat);
         MainMenu.runMenu(player, player.getAnimal(0));
     }
 }
