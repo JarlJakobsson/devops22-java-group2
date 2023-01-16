0495ccac4c42d0220ed14572e56a0a2264a4b7a9
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2023-01-16 14:10:46 +0100

    Added upgradeAnimal, setPrice, getPrice+price (all

--- demo/src/main/java/Y/Animal.java
+++ demo/src/main/java/Y/Animal.java
@@ -1,18 +1,32 @@
 package Y;
 
+import java.util.Scanner;
+import java.util.Random;
+
 /**
  * Animal superclass
  */
 abstract class Animal {
     private String name;
     private int milkValue;
+    private int price;
+    // private int shinyMultiplier = 1;
+    // private boolean isShiny = false;
 
     public Animal() {
-    }
+        }
 
     public int getMilkValue() {
         return milkValue;
     }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    } 
 
     public String getName() {
         return name;
@@ -24,5 +38,9 @@ abstract class Animal {
 
     public void setMilkValue(int milkValue) {
         this.milkValue = milkValue;
+    }
+
+    public void upgradeAnimal(int milkValue) {
+        setMilkValue(getMilkValue() * 2);
     }
 }
