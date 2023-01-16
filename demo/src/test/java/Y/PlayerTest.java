368a5c626eb2959d0cb1da42356318734374b957
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2023-01-16 14:15:52 +0100

    Added some player tests (all)

--- demo/src/test/java/Y/PlayerTest.java
+++ demo/src/test/java/Y/PlayerTest.java
@@ -1,19 +1,16 @@
 package Y;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class PlayerTest {
-    /**
-     * Rigorous Test :-)
-     */
+
     Player player = new Player("Milkman");
     Cow cow = new Cow();
-    GigaGoat gigaGoat = new GigaGoat();
+    Almond almond = new Almond();
 
     @Test
     public void milkTest() {
@@ -22,16 +19,32 @@ public class PlayerTest {
     }
 
     @Test
-    public void upgradeMilkingSkill() {
-        player.upgradeMilkingSkill(1);
-        player.milkAnimal(cow);
-        assertEquals(player.getBucket(), 2);
+    public void upgradeMilkingSkillTest() {
+        player.setBucket(5);
+        player.upgradeMilkingSkill();
+        assertEquals(player.getMilkingSkill(), 2);
     }
 
-    // Tests if bucket will be set to bucket size if getting more milk than bucket size
+    // Tests if bucket will be set to bucket size if getting more milk than bucket
+    // size
     @Test
     public void testFullBucket() {
-        player.milkAnimal(gigaGoat);
+        player.milkAnimal(almond);
         assertEquals(player.getBucket(), player.getBucketsize());
+    }
+
+    // Tests if buyAnimal function works
+    boolean working = false;
+
+    @Test
+    public void testBuyAnimal() {
+        player.buyAnimal(cow);
+
+        for (Animal animal : player.getAnimalList()) {
+            if (animal.getName() == "Cow") {
+                this.working = true;
+                assertTrue(this.working);
+            }
+        }
     }
 }
